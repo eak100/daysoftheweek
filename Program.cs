6891b35b9910a8a66f5caa3d51294fead5ae09e0
Erdi Akyüz <erdiakyuz@gmail.com>
2020-05-26 16:26:21 +0200

    Array example

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace daysoftheweek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string monday = "Monday";
+
+			string[] daysOfWeek =
+			{
+			/* The square brackets tell the compiler this is an array of string
+			 */
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday",
+			"Sunday"
+			};
+		}
+    }
+}
